b4092fc3556b3d4d0f08771f57e4529c266ced09
untitled95 <jackie950212@gmail.com>
2019-10-21 14:19:24 -0500

    ignore key

--- server.js
+++ server.js
@@ -11,10 +11,6 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/',async(req,res)=>{
-    res.send(200);
-})
-
 //this api only debugging purpose, do not use this one in production 
 app.get('/api/users', async (req, res) => {
     const users = await User.find()
